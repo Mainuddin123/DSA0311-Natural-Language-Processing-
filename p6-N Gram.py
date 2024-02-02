fc8904304d21b6df8f59cea68e4344bf3b4d40e8
Sk Khaja Mainuddin <112795691+Mainuddin123@users.noreply.github.com>
2024-02-02 09:22:44 +0530

    Update p6-N Gram.py

--- p6-N Gram.py
+++ p6-N Gram.py
@@ -4,7 +4,7 @@ words = sample_text.split()
 bigrams = [(words[i], words[i+1]) for i in range(len(words)- 1)]
 generated_text = []
 current_word = random.choice(words)  
-for _ in range(50):  # Generating 50 words
+for _ in range(50):  
     next_candidates = [b[1] for b in bigrams if b[0] == current_word]
     if next_candidates:
         next_word = random.choice(next_candidates)
